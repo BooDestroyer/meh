b556834d877223762857b87e278812df2be94935
BooDestroyer <44749929+BooDestroyer@users.noreply.github.com>
2018-12-29 23:18:29 +0200

    Add files via upload

--- main2.c
+++ main2.c
@@ -2,67 +2,57 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-float average1(int x[], int y);
+void swap(int *x, int *y, int *z);
+
 int main(int argc, char *argv[]) {
-	int array[10];
+	int grades[2][3];
 	int i=0;
-	int k=0;
-	int h=0;
-	float avg;
-	
-	for(i=0; i<=9; i++) {
-		printf("Give me the grades of your students\n");
-		scanf("%d", &array[i]);
-		if(array[i]<0 || array[i]>10) {
+	int j=0;
+    int count1=0;
+    int count2=0;
+	for(i=0; i<=1; i++) {
+		if(i==0) {
+			printf("Give the grades of the first student\n");
+		} else if(i==1) {
+			printf("Give the grades of the second student\n");
+		}
+		for(j=0; j<=2; j++) {
 			do {
-				if(array[i]==-1) {
-					break;
+				scanf("%d",(*(grades + i)+j));
+				if(*(*(grades +i)+j)<0 || *(*(grades +i)+j)>10) {
+					printf("Please give me the right grade\n");
 				}
-				printf("give a number to 0-10\n");
-				scanf("%d",&array[i]);
-				if(array[i]==-1) {
-					//printf("you gave minus 1\n");
-					break;
-				}
-			} while(array[i]<0 || array[i]>10);
-		}
-		
-
-		if(array[i]==-1) {
-			printf("you gave minus 1\n");
-			break;
-		}
-         h++;
-	}
-	int parray[h];
-	int farray[h-1];
-	for(i=0;i<=h;i++){
-		if(array[i]>=5){
-			parray[i]=array[i];
-			printf("parray %d\n",parray[i]);
-		}else if(array[i]<5 && array[i]!=-1){
-			if(array[i]==-1){
-				farray[i]=0;
-			}
-			farray[i]=array[i];
-			printf("farray %d\n",farray[i]);
+			} while(*(*(grades +i)+j)<0 || *(*(grades +i)+j)>10);
 		}
 	}
-	avg=average1(array,h);
-	printf("the average of th people who passed is %.2f\n",avg);
-
 	
+	 j=0;
+	for(j=0;j<=2;j++){
+		if(grades[0][j]>=5){
+			count1++;
+		}
+		if(grades[1][j]>=5){
+			count2++;
+		}
+	}
+	if(count1==3){
+		swap(&grades[0][0],&grades[0][1],&grades[0][2]); 
+	 printf("The grades of the first student from higher to lower is: %d, %d, %d\n",grades[0][0],grades[0][1],grades[0][2]);
+	}else if(count1==0){
+		//user define 2
+	}
+	if(count2==3){
+		swap(&grades[1][0],&grades[1][1],&grades[1][2]);
+		printf("The grades of the second student from higher to lower is: %d, %d, %d\n",grades[1][0],grades[1][1],grades[1][2]);
+	}else if(count2==0){
+		//user define 2
+	}
+	
+		 
 	return 0;
 }
-float average1(int x[],int y){
-	int p=0;
-	int i=0;
-	for(i=0;i<=y;i++){
-		if(x[i]>=5){
-		
-		p=p+x[i];
-	}
-	}
-	p= (float) p/(y-1);
-	return p;
+void swap(int *x, int *y, int *z){
+	
+
 }
+
